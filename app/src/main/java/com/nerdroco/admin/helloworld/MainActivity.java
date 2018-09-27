63f67a87a45d3734b93c072fcb29e4513bf8bcae
Dayo <admin@nerdroco.com>
2018-09-27 14:39:49 -0700

    Updated Menu to take action when item is clicked on.

--- app/src/main/java/com/nerdroco/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/nerdroco/admin/helloworld/MainActivity.java
@@ -3,18 +3,35 @@ package com.nerdroco.admin.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // set the field to the value of the main text element
+        greetView = findViewById(R.id.greeting);
     }
 
+    // Inflates the menu when its clicked on
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    // Performs this action if we select the option from the menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.action_bye){
+            greetView.setText(R.string.bye);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
